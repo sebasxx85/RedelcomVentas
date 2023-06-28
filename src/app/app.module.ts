57d04fdbe6f845958f05296fc4b30ada41c6b904
Sebastian Sepulveda <isurus.1313@gmail.com>
2023-06-28 18:18:09 -0400

    Primer commit proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { ContenidoComponent } from './componentes/contenido/contenido.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    MenuComponent,
+    FooterComponent,
+    ContenidoComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule
